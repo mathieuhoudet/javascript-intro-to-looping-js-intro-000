222fc5d35761dd5b5b08083a6d04e489a3a698f5
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-04-06 22:20:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,6 +5,7 @@ function forLoop(array){
     array.push("I am 1 strange loop");
   }else{
     array.push("I am" + i + "strange loop");
-  } console.log("I am" + i + "strange loop.");
+  }
+  return array;
 }
 }
