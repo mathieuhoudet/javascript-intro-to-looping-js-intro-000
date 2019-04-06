f9275be444eaa1ad971cd6dfa6b00c47c5d57be1
Mathieu Houdet <mathieu.houdet@gmail.com>
2019-04-06 22:18:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,8 @@
-for (let = 0; i > 25; i++){
+function forLoop(array){
+
+  for (let = 0; i > 25; i++){
   if (i == 1){
     console.log("I am 1 strange loop.");
   } console.log("I am" + i + "strange loop.");
+}
 }
